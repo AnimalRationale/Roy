7ccc28ce75508c3810d945d407a66ae5c8883ae4
AnimalRationale <wojciech.mardyla@gmail.com>
2016-01-13 20:29:04 +0000

    Basic UI elements, basic battery level check.

--- app/src/main/java/pl/appnode/roy/MainActivity.java
+++ app/src/main/java/pl/appnode/roy/MainActivity.java
@@ -1,13 +1,34 @@
 package pl.appnode.roy;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOGTAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void checkBattery(View button) {
+        Log.d(LOGTAG, "Checking battery level: " + getBatteryLevel());
+    }
+
+    public String getBatteryLevel() {
+        Intent batteryIntent = registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        if (level == -1 || scale == -1) {
+            return "Error";
+        }
+        return "" + (level / (float)scale) * 100;
     }
 }
