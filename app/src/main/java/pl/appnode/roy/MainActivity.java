2642e5f8ac2980d49f2ca56f073807e06e0bace0
AnimalRationale <wojciech.mardyla@gmail.com>
2016-01-14 15:25:55 +0000

    Check if device is plugged (USB, AC, wireless).

--- app/src/main/java/pl/appnode/roy/MainActivity.java
+++ app/src/main/java/pl/appnode/roy/MainActivity.java
@@ -5,13 +5,17 @@ import android.content.IntentFilter;
 import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOGTAG = "MainActivity";
+
+    private static final int BATTERY_NOT_PLUGGED = 0;
+    private static final int BATTERY_PLUGGED_AC = 1;
+    private static final int BATTERY_PLUGGED_USB = 2;
+    private static final int BATTERY_PLUGGED_WIRELESS = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +29,37 @@ public class MainActivity extends AppCompatActivity {
                 + getString(R.string.percent));
     }
 
-    public int getBatteryLevel() {
+    private Intent getBattery() {
         Intent batteryIntent = registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-        int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        return batteryIntent;
+    }
+
+    private int getBatteryLevel() {
+        int level = getBattery().getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = getBattery().getIntExtra(BatteryManager.EXTRA_SCALE, -1);
         if (level == -1 || scale == -1) {
             return -1;
         }
         return (int)((level / (float)scale) * 100);
+    }
+
+    private int getBatteryPluggedStatus() {
+        int status = getBattery().getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        int batteryPluggedStatus;
+
+        switch (status) {
+            case BatteryManager.BATTERY_PLUGGED_USB:
+                batteryPluggedStatus = BATTERY_PLUGGED_USB;
+                break;
+            case BatteryManager.BATTERY_PLUGGED_AC:
+                batteryPluggedStatus = BATTERY_PLUGGED_AC;
+                break;
+            case BatteryManager.BATTERY_PLUGGED_WIRELESS:
+                batteryPluggedStatus = BATTERY_PLUGGED_WIRELESS;
+                break;
+            default:
+                batteryPluggedStatus = BATTERY_NOT_PLUGGED;
+        }
+        return batteryPluggedStatus;
     }
 }
