97fa07715ff7a1a7d3ad90eb465546d833c1f1c0
AnimalRationale <wojciech.mardyla@gmail.com>
2016-01-19 23:33:47 +0000

    Class defining structure for battery data object.

---
+++ app/src/main/java/pl/appnode/roy/BatteryItem.java
@@ -0,0 +1,13 @@
+package pl.appnode.roy;
+
+/**
+ * Defines structure of items holding battery information.
+ */
+public class BatteryItem {
+    String batteryDeviceId;
+    String batteryDeviceName;
+    float batteryCheckTime;
+    int batteryLevel;
+    int batteryPluggedStatus;
+    int batteryChargingStatus;
+}
