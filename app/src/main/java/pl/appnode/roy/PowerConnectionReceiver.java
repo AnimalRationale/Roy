998e1d4d3325453f2295585ef43fdfa088b6fd56
AnimalRationale <wojciech.mardyla@gmail.com>
2016-05-26 20:10:11 +0100

    Power connection/disconnection broadcast receiver.

---
+++ app/src/main/java/pl/appnode/roy/PowerConnectionReceiver.java
@@ -0,0 +1,14 @@
+package pl.appnode.roy;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class PowerConnectionReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent serviceIntent = new Intent(context, RemoteUpdateService.class);
+        context.startService(serviceIntent);
+    }
+}
