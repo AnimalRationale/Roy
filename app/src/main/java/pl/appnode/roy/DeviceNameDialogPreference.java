2b0955cb398aeb10651f09ee810385d7d0edb45b
AnimalRationale <wojciech.mardyla@gmail.com>
2016-06-09 21:38:47 +0100

    Init for custom edit text preference dialog (workaround for not properly set app theme in standard dialog).

---
+++ app/src/main/java/pl/appnode/roy/DeviceNameDialogPreference.java
@@ -0,0 +1,41 @@
+package pl.appnode.roy;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+public class DeviceNameDialogPreference extends EditTextPreference {
+
+    public DeviceNameDialogPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public DeviceNameDialogPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+        final Resources resources = getContext().getResources();
+        final Window window = getDialog().getWindow();
+        final int titleColor = ContextCompat.getColor(getContext(),R.color.colorPrimaryMaterial);
+        final int titleId = resources.getIdentifier("alertTitle", "id", "android");
+        final View title = window.findViewById(titleId);
+        if (title != null) {
+            ((TextView) title).setTextColor(titleColor);
+        }
+        final int titleDividerId = resources.getIdentifier("titleDivider", "id", "android");
+        final View titleDivider = window.findViewById(titleDividerId);
+        if (titleDivider != null) {
+            titleDivider.setBackgroundColor(titleColor);
+        }
+        window.getDecorView().setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorDarkGrey));
+    }
+}
